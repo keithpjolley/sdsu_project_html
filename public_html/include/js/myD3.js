c4a62dcf6ff6ac9624061f0b4bc8099de2322efd
keith p. jolley <keithpjolley@gmail.com>
2013-05-25 11:37:01 -0700

    updating css

--- public_html/include/js/myD3.js
+++ public_html/include/js/myD3.js
@@ -56,12 +56,14 @@
                      "   page.rank: " + d.pr        + "<br/>" +
                      " eigenvector: " + d.evcent);
               })
-          });
+                 .style("left", (d3.event.pageX - 0)  + "px")
+                 .style("top",  (d3,event.pageY - 28) + "px");
+          })
           .on("mouseout", function(d) {
               div.transition()
                  .duration(500)
                  .style("opacity", 0);
-              });
+          });
 //           node.append("title")
 //             .text(function(d) { return (
 //              d.name + "\n" +
