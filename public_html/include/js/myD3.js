b68fefec309a7ee9f627106f7cbf0886215c60c4
keith p. jolley <keithpjolley@gmail.com>
2013-05-25 23:17:44 -0700

    i hate tooltips

--- public_html/include/js/myD3.js
+++ public_html/include/js/myD3.js
@@ -46,13 +46,13 @@
           .style("stroke", function(d) { return ((d.isperson==1) ? "white" : "grey")}) 
           .style("stroke-width", function(d) { return Math.max(d.radius/10,1)}) 
           .append("title")
-             .text(function(d) { return (
+          .call(force.drag)
+          .text(function(d) { return (
               d.name + "\n" +
               "   community: " + d.community + "\n" +
               "   page.rank: " + d.pr        + "\n" +
-              " eigenvector: " + d.evcent);
-          })
-          .call(force.drag);
+              " eigenvector: " + d.evcent)
+          });
       force.on("tick", function () {
         var q = d3.geom.quadtree(graph.nodes);
         var i = 0;
