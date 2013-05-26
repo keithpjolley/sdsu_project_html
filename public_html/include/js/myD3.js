09339efaae9ecc0f420eeb162e7c01c0f3699e95
keith p. jolley <keithpjolley@gmail.com>
2013-05-26 03:23:39 -0700

    i really hate tooltips

--- public_html/include/js/myD3.js
+++ public_html/include/js/myD3.js
@@ -73,7 +73,7 @@
                var yPosition = parseFloat(d3.select(this).attr("cy")) + 0;
                //Update the tooltip position and value
                d3.select("#tooltip")
-                 .style("background-color", function(d) { return color(d.community)})
+                 .style("background-color", color(d.community))
                  .style("left", xPosition + "px")
                  .style("top",  yPosition + "px")
                  // these need to match those in function cgi-bin/tw.pl:tooltipper
